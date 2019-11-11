60f94be591b64f117655b03571b82dd8a25fddb0
tronsez <43092154+tronsez@users.noreply.github.com>
2019-11-11 15:59:06 +0100

    Add files via upload

---
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -0,0 +1,88 @@
+package com.gildedrose;
+
+class GildedRose {
+    Item[] items;
+
+    public GildedRose(Item[] items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (int i = 0; i < items.length; i++) {
+            doUpdateQuality(items[i]);
+        }
+    }
+
+    private void doUpdateQuality(Item item) {
+        switch (item.name) {
+            case "Aged Brie":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+                break;
+            case "Backstage passes to a TAFKAL80ETC concert":
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+
+                    if (item.sellIn < 11) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+
+                    if (item.sellIn < 6) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    }
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    item.quality = 0;
+                }
+                break;
+            case "Sulfuras, Hand of Ragnaros":
+
+                break;
+
+            case "Conjured Mana Cake":
+                if (item.quality > 0) {
+                    item.quality = item.quality - 2;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 2;
+                    }
+                }
+                break;
+
+            default:
+                if (item.quality > 0) {
+                    item.quality = item.quality - 1;
+                }
+
+                item.sellIn = item.sellIn - 1;
+
+                if (item.sellIn < 0) {
+                    if (item.quality > 0) {
+                        item.quality = item.quality - 1;
+                    }
+                }
+                break;
+        }
+    }
+
+}
