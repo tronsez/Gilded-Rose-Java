d9e375f719f48a1fa6456ff817bb8c6eb59fa00d
tronsez <43092154+tronsez@users.noreply.github.com>
2019-11-17 18:50:38 +0100

    Update GildedRoseTest.java

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,1 +1,30 @@
-***
+package com.gildedrose;
+
+import static org.junit.Assert.*;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.combinations.CombinationApprovals;
+import org.junit.Test;
+
+public class GildedRoseTest {
+
+    @Test
+    public void updateQuality() {
+
+        CombinationApprovals.verifyAllCombinations(
+                this::doUpdateQuality,
+                new String[]{"foo", "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros", "Conjured Mana Cake"},
+                new Integer[]{-1, 0, 2, 6, 11},
+                new Integer[]{0, 1, 49, 50});
+    }
+
+    private String doUpdateQuality(String name, int sellIn, int quality) {
+        Item[] items = new Item[] { new Item(name, sellIn, quality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        return app.items[0].toString();
+    }
+
+}
+
+
